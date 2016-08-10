cbe1a084510a931f1b1ca7b6579c67d0911560c7
bill wood <billwood.dev@gmail.com>
2016-08-10 15:51:38 -0400

    modify gulp file to start production code repo

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,49 @@
 
 var gulp = require('gulp');
 
-gulp.task('default', function(){
-    
+var connect = require('gulp-connect');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-clean');
+
+gulp.task('lint', function (){
+    gulp.src(['./app/**/*.js','!./app/bower_components/**'])
+        .pipe(jshint({esversion:6}))
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
 });
+
+gulp.task('clean', function (){
+    gulp.src('./dist/*')
+        .pipe(clean({force:true}));
+});
+
+gulp.task('minify-css', function (){
+    var opts = {comments:true, spare:true};
+    gulp.src(['./app/**/*.css','!./app/bower_components/**'])
+        .pipe(minifycss(opts))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify-js', function (){
+    gulp.src(['./app/**/*.js','!./app/bower_components/**'])
+        .pipe(uglify({
+            // inSourceMap:
+            // outSourceMap: "app.js/map"
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('copy-bower-components', function (){
+    gulp.src('./app/bower_components/**')
+        .pipe(gulp.dest('dist/bower_components'));
+});
+
+gulp.task('copy-html-files', function (){
+    gulp.src('./app/**/*.html')
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['lint']);
+
+gulp.task('build', ['lint', 'clean', 'minify-css', 'minify-js', 'copy-bower-components', 'copy-html-files']);
