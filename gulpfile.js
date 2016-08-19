7f4900f2b715b751df659ee4eac224f2bd4d3b87
bill wood <billwood.dev@gmail.com>
2016-08-19 19:27:47 -0400

    Adding angular gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,28 @@
 
 var gulp = require('gulp');
 
-var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
-var minifycss = require('gulp-clean');
+var concat = require('gulp-concat');
+var templates = require('gulp-angular-templatecache');
+var minifyHTML = require('gulp-minify-html');
+var clean = require('gulp-clean');
+
+var eslint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
+//var minifycss = require('gulp-clean');
 var Server = require('karma').Server;
 
+var dist = './dist';
+
+//Check to make sure coding is up to standards
 gulp.task('lint', function (){
-    gulp.src(['./app/**/*.js','!./app/bower_components/**'])
-        .pipe(jshint({esversion:6}))
-        .pipe(jshint.reporter('default'))
-        .pipe(jshint.reporter('fail'));
+    return gulp.src(['./src/**/*.js','!./src/client/bower_components/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
 });
 
+//Angular UNIT Test
 gulp.task('tdd', ['lint'], function (done) {
   new Server({
     configFile: __dirname + '/karma.conf.js',
@@ -21,35 +31,44 @@ gulp.task('tdd', ['lint'], function (done) {
   }, done).start();
 });
 
+//Convert Anuglar html templates to cache
+gulp.task('templates', function () {
+    gulp.src(['./src/client/**/*.template.html'])
+        .pipe(minifyHTML({
+            quotes: true
+        }))
+        .pipe(templates('templates.js'))
+        .pipe(gulp.dest(dist));
+});
+
+// Concat and uglify all your JavaScript
+gulp.task('minify-js', ['templates'], function() {
+  gulp.src(['./src/client/**/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('clean', function (){
     gulp.src('./dist/*')
         .pipe(clean({force:true}));
 });
 
-gulp.task('minify-css', function (){
-    var opts = {comments:true, spare:true};
-    gulp.src(['./app/**/*.css','!./app/bower_components/**'])
-        .pipe(minifycss(opts))
-        .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('minify-js', function (){
-    gulp.src(['./app/**/*.js','!./app/bower_components/**'])
-        .pipe(uglify({
-            // inSourceMap:
-            // outSourceMap: "app.js/map"
-        }))
-        .pipe(gulp.dest('./dist/'));
-});
+//gulp.task('minify-css', function (){
+//    var opts = {comments:true, spare:true};
+//    gulp.src(['./app/**/*.css','!./app/bower_components/**'])
+//        .pipe(minifycss(opts))
+//        .pipe(gulp.dest('./dist/'));
+//});
 
 gulp.task('copy-bower-components', function (){
-    gulp.src('./app/bower_components/**')
+    gulp.src('./src/client/bower_components/**')
         .pipe(gulp.dest('dist/bower_components'));
 });
 
 gulp.task('copy-html-files', function (){
-    gulp.src('./app/**/*.html')
-        .pipe(gulp.dest('dist/'));
+    gulp.src('./src/client/index.html')
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('default', ['tdd']);
