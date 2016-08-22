027cf164887f0b1ad8c3e6ce5762684fecdee6ac
bill wood <billwood.dev@gmail.com>
2016-08-22 11:48:45 -0400

    game-compare tdd for games array

--- src/client/app/game-compare/game-compare.component.spec.js
+++ src/client/app/game-compare/game-compare.component.spec.js
@@ -4,8 +4,32 @@ describe('gameCompare', function(){
     beforeEach(module('gameCompare'));
 
     describe('GameCompareController', function(){
-        describe('Game Compare Initialize', function(){
 
+        var _gamesData = [
+            {'id':'379720'},
+            {'id':'377160'},
+            {'id':'234140'}
+        ];
+        var underTest, $httpBackend;
+
+        beforeEach(inject(function ($componentController, _$httpBackend_){
+            $httpBackend = _$httpBackend_;
+            underTest = $componentController('gameCompare');
+            underTest.games = _gamesData;
+            //mockUtilSvc = Game;
+
+            $httpBackend.expectGET('api/games').respond(_gamesData);
+            
+        }));
+
+        describe('Game Compare Initialize', function(){
+            it('Should retrieve games data ', function() {
+                jasmine.addCustomEqualityTester(angular.equals);
+
+                $httpBackend.flush();
+
+                expect(underTest.games).toEqual(_gamesData);
+            });
         });
     });
 });
