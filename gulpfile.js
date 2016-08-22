d8dc368ab2a396e9ddffe780844c54097e2bac78
bill wood <billwood.dev@gmail.com>
2016-08-22 11:07:12 -0400

    refactoring gulp file. tidying up

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 'use strict';
 
+//Gulp task runner
 var gulp = require('gulp');
 
+//Gulp assistants
+var gutil = require('gulp-util');
+var clean = require('gulp-clean');
+
+//Test and verifying javascript w/ unit tests
 var eslint = require('gulp-eslint');
 var Server = require('karma').Server;
 
-var clean = require('gulp-clean');
-
+//replace blocks of development sources with production mini files
 var htmlreplace = require('gulp-html-replace');
 
+//store HTML templates within Angular templateCache
 var templates = require('gulp-angular-templatecache');
+//might remove this
 var minifyHTML = require('gulp-minify-html');
 
+//Bundle angular app code into one production file.
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
@@ -19,27 +27,44 @@ var ngAnnotate = require('gulp-ng-annotate');
 var bytediff = require('gulp-bytediff');
 var babel = require('gulp-babel');
 
+//directory sources
 var pkg = {
     paths: {
-        config: {
-            karma: __dirname + '/karma.conf.js'
-        },
-        src: {
-            js: 'src/**/*.js',
-            css: 'src/**/*.css',
-            templates: 'src/client/**/*.template.html'
-        },
-        dist: {
-            js: './dist/main.min.js',
-            css: './dist/style.min.css',
-            templates: './dist/template.js'            
-        }
+        karma_cfg: __dirname + '/karma.conf.js',
+        client_src: 'src/client/',
+        client_dist: 'dist/client/',
+        server_script_src: 'src/server/index.js',
+        karma_script_src: 'src/client/app/**/*.spec.js',
+        bower_script_src: 'src/client/bower_components/**',
+        bower_script_dest: 'dist/client/bower_components',
+        client_template_src: 'src/client/index.html',
+        client_style_dest: 'style.min.css',
+        client_script_dest: [
+            'templates.min.js', 
+            'app.min.js'
+        ],
+        angular_script_src: [
+            'src/client/app/*.module.js',
+            'src/client/app/*.config.js',
+            'src/client/app/core/*.module.js',
+            'src/client/app/core/**/*.module.js',
+            'src/client/app/core/**/*.service.js',
+            'src/client/app/**/*.module.js',
+            'src/client/app/**/*.component.js'
+        ],
+        angular_script_dest: 'app.min.js',
+        angular_template_src: 'src/client/app/**/*.template.html',
+        angular_template_dest: 'templates.min.js'
     }
-};
+}
 
 //Check to make sure coding is up to standards
 gulp.task('lint', function (){
-    return gulp.src(['src/**/*.js', '!**/bower_components/**/*.js'])
+    return gulp.src([].concat(
+            pkg.paths.angular_script_src,
+            pkg.paths.karma_script_src,
+            pkg.paths.server_script_src
+        ))
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failOnError());
@@ -47,36 +72,38 @@ gulp.task('lint', function (){
 
 //Angular UNIT Test
 gulp.task('tdd', ['lint'], function (done) {
-  new Server({
-    configFile: pkg.paths.config.karma,
+  return new Server({
+    configFile: pkg.paths.karma_cfg,
     singleRun: true
   }, done).start();
 });
 
 //Clean the dist folder before build
 gulp.task('clean', function (){
-    gulp.src(['dist/*', 'dist/**/*'])
-        .pipe(clean({force:true}));
+    return gulp.src(pkg.paths.client_dist)
+        .pipe(clean({force:true,read: false}));
 });
 
 //Convert Anuglar html templates to cache
-gulp.task('templates', ['clean'], function () {
-    gulp.src([pkg.paths.src.templates])
-        .pipe(minifyHTML({
-            quotes: true
+gulp.task('cache-templates', ['clean'], function () {
+    return gulp.src(pkg.paths.angular_template_src)
+        .pipe(templates(pkg.paths.angular_template_dest, {
+            standalone: true,
+            transformUrl: function (url){
+                return url;
+            }
         }))
-        .pipe(templates('templates.js'))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest(pkg.paths.client_dist));
 });
 
 //Application JS files, 
-gulp.task('js', ['clean', 'tdd', 'templates'], function () {
-    return gulp.src(['src/client/**/*.js', '!**/*(.test|.spec).js', '!**/bower_components/**/*.js'])
+gulp.task('minify-js', ['clean'], function () {
+    return gulp.src(pkg.paths.angular_script_src)
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['es2015']
         }))
-        .pipe(concat('app.min.js', {newLine: ';'}))
+        .pipe(concat(pkg.paths.angular_script_dest, {newLine: ';'}))
         .pipe(ngAnnotate({
             add: true
         }))
@@ -84,27 +111,25 @@ gulp.task('js', ['clean', 'tdd', 'templates'], function () {
         .pipe(uglify({mangle: true}))
         .pipe(bytediff.stop())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(pkg.paths.client_dist));
 });
 
 //move the bower files to dist
 gulp.task('copy-bower-components', ['clean'], function (){
-    gulp.src('src/client/bower_components/**')
-        .pipe(gulp.dest('dist/bower_components'));
+    return gulp.src(pkg.paths.bower_script_src)
+        .pipe(gulp.dest(pkg.paths.bower_script_dest));
 });
 
 //move the index to dist and point to minify css & js
 gulp.task('copy-index', ['clean'], function (){
-    gulp.src('src/client/index.html')
+    return gulp.src(pkg.paths.client_template_src)
         .pipe(htmlreplace({
-            'css': 'style.min.css',
-            'js': 'app.min.js'
+            'css': pkg.paths.client_style_dest,
+            'js': pkg.paths.client_script_dest
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(pkg.paths.client_dist));
 });
 
-gulp.task('default', ['lint', 'tdd']);
+gulp.task('test', ['tdd']);
 
-gulp.task('build', ['default', 'js', 'templates', 'copy-bower-components', 'copy-index']);
-
-
+gulp.task('build', ['copy-bower-components', 'cache-templates', 'minify-js', 'copy-index']);
