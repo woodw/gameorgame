a6af3e1d92b856fa24c76ee38a283e98af7106e2
bill wood <billwood.dev@gmail.com>
2016-09-02 16:32:11 -0400

    add route api/person to return person data

--- src/server/development.js
+++ src/server/development.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 const fs = require('fs');
 
-const appPersonGames = retrieveJSON(__dirname + '/data_mock/app_person_games.json');
-const appPersonGameDetails = retrieveJSON(__dirname + '/data_mock/app_person_game_details.json');
+var appPerson = JSON.parse(fs.readFileSync(__dirname + '/data_mock/app_person.json', 'utf8'));
+var appPersonGames = JSON.parse(fs.readFileSync(__dirname + '/data_mock/app_person_games.json', 'utf8'));
+var steamPerson = JSON.parse(fs.readFileSync(__dirname + '/data_mock/steam_person.json', 'utf8'));
+var steamPersonGames = JSON.parse(fs.readFileSync(__dirname + '/data_mock/steam_person_games.json', 'utf8'));
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(path.join(__dirname, '../client')));
@@ -14,6 +16,10 @@ app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
     response.render('index.html');
+});
+
+app.get('/api/person/:steamId', function (request, response){
+    response.json(appPersonGames);
 });
 
 app.get('/api/games/', function(request, response){
@@ -35,10 +41,3 @@ app.use(function(request, response){
 app.listen(app.get('port'), function() {
     //console.log('running on port', app.get('port'));
 });
-
-function retrieveJSON(path){
-    fs.readFile(path, 'utf8', function (err, data) {
-        return JSON.parse(data) || {};
-    });
-    return {};
-}
